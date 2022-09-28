231ebdf8fefb57d22f254ae15eedbcba59597ec0
Rwhytm <rajuwahyudi842@gmail.com>
2022-09-28 13:42:52 +0700

    connect with sql and add dunction insert

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/mamiana")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	fmt.Println("Success!")
+
+	insert, err := db.Query("INSERT INTO user VALUES('raju wahyudi pratama','23','laki','90')")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer insert.Close()
 	fmt.Println("Hello world")
 }
