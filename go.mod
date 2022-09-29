93ff23e2a38c4de2ee6aafd7c8294b31ddf4339c
Rwhytm <rajuwahyudi842@gmail.com>
2022-09-29 12:49:51 +0700

    done for send whatsapp

--- go.mod
+++ go.mod
@@ -2,4 +2,34 @@ module example.com/m/v2
 
 go 1.18
 
-require github.com/go-sql-driver/mysql v1.6.0 // indirect
+require (
+	cloud.google.com/go v0.104.0 // indirect
+	cloud.google.com/go/compute v1.10.0 // indirect
+	cloud.google.com/go/firestore v1.6.1 // indirect
+	cloud.google.com/go/iam v0.5.0 // indirect
+	cloud.google.com/go/storage v1.27.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.1.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.5.1 // indirect
+	github.com/googleapis/go-type-adapters v1.0.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/twilio/twilio-go v0.26.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20220927171203-f486391704dc // indirect
+	golang.org/x/oauth2 v0.0.0-20220909003341-f21342109be1 // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/api v0.98.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220927151529-dcaddaf36704 // indirect
+	google.golang.org/grpc v1.49.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,24 +1,75 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"reflect"
 
-	_ "github.com/go-sql-driver/mysql"
+	firebase "firebase.google.com/go"
+	"github.com/joho/godotenv"
+	twilio "github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
 )
 
+// }
 func main() {
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/mamiana")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
-	fmt.Println("Success!")
 
-	insert, err := db.Query("INSERT INTO user VALUES('raju wahyudi pratama','23','laki','90')")
+	// setEnv()
+	sa := option.WithCredentialsFile("./ServiceAccountKey.json")
+	app, err := firebase.NewApp(context.Background(), nil, sa)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalln(err)
 	}
-	defer insert.Close()
-	fmt.Println("Hello world")
+	client, err := app.Firestore(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	dsnap := client.Collection("user").Where("role", "==", "user").Documents(context.Background())
+	for {
+		doc, err := dsnap.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+		data := doc.Data()
+		nomorhp := data["nomorhp"]
+
+		fmt.Println(data["nomorhp"])
+		fmt.Println(reflect.TypeOf(nomorhp))
+		sendMessage("S", nomorhp.(string))
+	}
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer client.Close()
+	fmt.Println(os.Getenv("TWILIO_ACCOUNT_SID"))
+
+}
+
+func sendMessage(message, phone string) {
+	godotenv.Load("go.env")
+	client := twilio.NewRestClientWithParams(
+		twilio.ClientParams{
+			Username: os.Getenv("TWILIO_ACCOUNT_SID"),
+			Password: os.Getenv("TWILIO_AUTH_TOKEN"),
+		})
+	a := &openapi.CreateMessageParams{}
+	a.SetTo("whatsapp:" + phone)
+	a.SetFrom("whatsapp:+14155238886")
+	a.SetBody(message)
+
+	_, err := client.Api.CreateMessage(a)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Message sent successfully!")
+	}
 }
