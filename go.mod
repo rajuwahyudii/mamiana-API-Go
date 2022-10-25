ffe66dfbcab7e1743ef1fd976c85054c679abaaa
Rwhytm <rajuwahyudi842@gmail.com>
2022-10-25 10:06:58 +0700

    add some

--- go.mod
+++ go.mod
@@ -9,7 +9,13 @@ require (
 	cloud.google.com/go/iam v0.5.0 // indirect
 	cloud.google.com/go/storage v1.27.0 // indirect
 	firebase.google.com/go v3.13.0+incompatible // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.8.1 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
@@ -19,17 +25,27 @@ require (
 	github.com/googleapis/gax-go/v2 v2.5.1 // indirect
 	github.com/googleapis/go-type-adapters v1.0.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/twilio/twilio-go v0.26.0 // indirect
+	github.com/ugorji/go/codec v1.2.7 // indirect
 	go.opencensus.io v0.23.0 // indirect
-	golang.org/x/net v0.0.0-20220927171203-f486391704dc // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/net v0.1.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20220909003341-f21342109be1 // indirect
-	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/api v0.98.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220927151529-dcaddaf36704 // indirect
 	google.golang.org/grpc v1.49.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,22 +1,157 @@
 package main
 
 import (
-	"io"
-	// "log"
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-	// "os"
+	"strconv"
+	"time"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	"github.com/gin-gonic/gin"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World")
-}
-
+// }
 func main() {
-	// port := os.Getenv("PORT")
-	http.HandleFunc("/", hello)
-	// log.Print("Listening on :" + port)
-	// log.Fatal(http.ListenAndServe(":"+port, nil))
-	http.ListenAndServe(":3000", nil)
-	// defer client.Close()
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	tips := map[string]interface{}{
+		"1":  "Yth.Ibu Jangan lupa melakukan pemeriksaan kehamilan di Bidan setiap bulan atau jika mengalami tanda bahaya selama kehamilan.",
+		"2":  "Tanda bahaya dalam kehamilan: keluar darah dari jalan lahir, ketuban pecah sebelum tanda persalinan, bengkak seluruh tubuh, sakit kepala hebat, kejang, demam lebih dari 2 hari, berat badan tidak naik.",
+		"3":  "Selamat malam ibu. Jangan lupa minum obat tambah darah untuk mencegah anemia agar terhindar dari perdarahan banyak setelah melahirkan. Obat tambah darah tidak boleh diminum bersamaan dengan teh, kopi, susu karena dapat merusak khasiat obat.",
+		"4":  "Selamat pagi ibu. Semoga ibu dan bayi dalam keadaan sehat. Selama hamil makanlah apa yang ibu sukai, jangan dipantang agar ibu dan bayi dalam kandungan sehat.",
+		"5":  "Makanlah makanan dengan menu seimbang: nasi, sayur, lauk (ikan, tempe, telur, ayam), buah-buah, susu dan air putih minimal 8 gelas dalam sehari.",
+		"6":  "Selamat malam ibu. Sudahkah ibu minum obat tambah darahnya malam ini? Kalau belum, diminum ya bu agar ibu dan bayi tetap sehat dan terhindar dari perdarahan banyak setelah melahirkan. Apabila di pagi hari BAB ibu berwarna hitam, ibu jangan khawatir.",
+		"7":  "Tahukah ibu? Anemia pada kehamilan dapat menyebabkan keguguran, persalinan prematur, serta gangguan pertumbuhan janin.",
+		"8":  "Perlu dikenali beberapa tanda dan gejala anemia pada ibu hamil agar dapat dideteksi secara mandiri dan sedini mungkin: wajah, terutama kelopak mata dan bibir tampak pucat. Mata berkunang-kunang, serta sering mengalami pusing. Jangan disepelakan ya bu. Segera kungjungi pusat kesehatan terkait jka mengalami salah satu gejala diatas.",
+		"9":  "Perlu diketahui bu, bahwa dalam masa pandemi, konsumsi Tablet Tambah Darah  sangat penting, untuk mencegah anemia sekaligus meningkatkan kekebalan tubuh terhadap virus corona.",
+		"10": "Bagaimana sih Biar Tidak Anemia? Makan Makanan BERGIZI SEIMBANG, Terutama Tinggi Protein, Kaya zat besi Minum tablet tambah darah secara teratur.",
+		"11": "Ingat ya bu, JANGAN minum tablet tambah darah dengan teh, kopi, atau susu, karena akan MENGHAMBAT penyerapan zat besi.",
+		"12": "Efek samping yang mungkin dialami ibu hamil dari mengonsumsi tablet tambah darah yaitu nyeri/perih di ulu hati, mual serta tinja berwarna kehitaman (yang berasal dari sisa zat besi yang dikeluarkan oleh tubuh melalui feses). Efek samping tidak sama dialami oleh setiap orang dan akan hilang dengan sendirinya.",
+		"13": "Jangan lupa bu! Tes laboratorium yang wajib dilakukan adalah tes hemoglobin darah (Hb) untuk mengetahui apakah ibu hamil menderita anemia, golongan darah untuk mempersiapkan donor bila diperlukan kelak.",
+		"14": "Sebaiknya ibu tidur pakai kelambu, jangan memakai anti nyamuk (bakar atau semprot).",
+		"15": "Selamat pagi ibu. Semoga ibu dan bayi tetap dalam keadaan sehat. Melahirkanlah ke tenaga kesehatan: bidan praktik, puskesmas dan Rumah Sakit untuk menghindari terjadinya masalah saat melahirkan. Terima Kasih.",
+		"16": "Selamat pagi ibu. Semoga ibu dan bayi yang dikandung selalu dalam keadaan sehat. Sudah berapa bulan usia kehamilannya sekarang bu? Marilah kita mengenali tanda-tanda bahwa ibu akan melahirkan, yaitu: Sakit perut sampai ke pinggang, keluar lendir bercampur darah, keluar air ketuba, perut kencang, jalan lahir terbuka.",
+		"17": "Ibu hamil setiap kali makan harus mengonsumsi makanan yang mengandung protein, karbohidrat dan zat gizi mikro (vitamin dan mineral) agar terhindar dari resiko anemia karena pola makan yang kurang beragam dan tidak bergizi seimbang merupakan faktor utama penyebab anemia dalam kehamilan.",
+		"18": "Pada masa pandemi covid-19, ibu hamil perlu memperhatikan: Konsumsi makanan bergizi seimbang (isi priringku), Kebiasaan selalu mencuci tangan pakai sabun dan air mengalir (Perilaku Hidup Bersih dan Sehat, Aktivitas fisik ringan sesuai untuk ibu hamil, Minum air putih minimal 8 gelas sehari",
+		"19": "Bila perut ibu hamil terasa perih karena meminum tablet tambah darah, mual serta tinja berwarna kehitaman, tidak perlu khawatir karena tubuh akan menyesuaikan. Untuk meminimalkan efek samping tersebut, jangan minum tablet tambah darah  dalam kondisi perut kosong.",
+		"20": "Sebaiknya tablet tambah darah diminum setelah makan malam agar konsumsi tablet tambah darah dapat lebih efektif untuk mencegah anemia. Tablet tambah darah dapat dikonsumsi bersama makanan atau minuman yang mengandung Vitamin C seperti buah segar, sayuran dan jus buah, agar penyerapan zat besi didalam tubuh lebih baik.",
+		"21": "Yang harus dipersiapkan sebelum melahirkan: tahu tanggal melahirkan, siapa yang menolong melahirkan, siapkan uang, siapkan kendaraan, siapkan orang yang akan mendonorkan darah dan semua keperluan melahirkan.",
+		"22": "Tanda-tanda melahirkan: Sakit perut sampai ke pinggang, keluar lendir bercampur darah, keluar air ketuban, perut kencang, jalan lahir tebuka.",
+		"23": "Tanda-tanda bahaya waktu melahirkan:  bayi tidak keluar dalam 12 jam dari perut sakit, perdarahan banyak dari jalan lahir, tali pusat dan tangan bayi keluar dan jalan lahir, ibu tidak kuat mengedan atau mengalami kejang, air ketuban keruh atau busuk.",
+		"24": "Tanda bahaya bayi baru lahir: bayi tidak mau menyusu, kejang, lemah, sesak nafas, pusat kemerahan, demam tinggi atau badan terasa dingin.",
+		"25": "Yang harus dihindari selama kehamilan: Kerja berat, Merokok dan terpapar asap rokok selama kehamilan karena akan mengganggu pertumbuhan janin, Mengonsumsi minuman yang mengandung soda, alkohol dan lain lain, Tidur terlentang pada hamil tua, Mengonsumsi obat tanpa resep dokter apabila ada keluhan",
+		"26": "Persiapkan pendonor yang memiliki golongan darah yang sama dengan ibu bersalin, upayakan pendonor berasal dari orang terdekat (keluarga atau tetangga).",
+		"27": "Bersama dengan suami lakukan stimulasi janin dengan cara: Stimulasi suara, berbicara dengan janin sejak hamil muda dengan kata-kata yang lemah lembut dan positif. Stimulasi raba, lakukan sentuhan dengan cara mengusap perut ibu hamil sesering mungkin.",
+		"28": "Selamat pagi ibu. Jangan lupa ke posyandu untuk mengetahui kesehatan ibu dan bayi di dalam kandungan.",
+		"29": "Simpan tablet tambah darah di tempat yang kering, terhindar dari sinar matahari langsung, jauh dari jangkauan anak dan setelah dibuka harus ditutup kembali dengan rapat tablet Tambah darah yang sudah berubah warna sebaiknya tidak di minum (warna asli: merah darah).",
+		"30": "Selamat pagi ibu. Tablet tambah darah tidak menyebabkan tekanan darah tinggi atau kelebihan darah.",
+	}
+	// fmt.Println(tips["1"])
+	//Use Credential Firebase
+	sa := option.WithCredentialsFile("./ServiceAccountKey.json")
+	app, err := firebase.NewApp(context.Background(), nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	client, err := app.Firestore(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	dsnap := client.Collection("user").Where("role", "==", "user").Documents(context.Background())
+
+	for {
+		doc, err := dsnap.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+		data := doc.Data()
+		nomorhp := data["nomorhp"]
+		nomorhp2 := nomorhp.(string)
+		hari := int(data["hari"].(int64))
+
+		id := data["id"].(string)
+		time := time.Now().Format("15:04")
+		schedule, err := client.Collection("user").Doc(id).Collection("day").Doc("day" + strconv.Itoa(hari)).Get(context.Background())
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		deadline := schedule.Data()
+
+		if time == "19:20" {
+			if deadline["status"].(string) == "belum" {
+				client.Collection("user").Doc(id).Collection("day").Doc("day"+strconv.Itoa(hari)).Set(context.Background(), map[string]interface{}{
+					"status": "tidak",
+				}, firestore.MergeAll)
+			}
+			client.Collection("user").Doc(id).Collection("day").Doc("day"+strconv.Itoa(hari)).Set(context.Background(), map[string]interface{}{
+				"status": "tidak",
+			}, firestore.MergeAll)
+			if hari < 30 {
+				client.Collection("user").Doc(id).Set(context.Background(), map[string]interface{}{
+					"hari": hari + 1,
+				}, firestore.MergeAll)
+			}
+		}
+
+		if time == "20:00" {
+			test("Selamat malam ibu..\nIngin mengingatkan sekarang sudah memasuki jam 20.00 waktunya ibu untuk meminum obat. Segera melakukan konfirmasi melalui aplikasi Mamiana dengan mengupload obat yang akan diminum hari ini\nTerimakasih", nomorhp.(string))
+			test("Tips : \n\n"+tips[strconv.Itoa(hari)].(string), nomorhp2)
+		}
+
+	}
+	if err != nil {
+		log.Fatalln(err)
+	}
+	r.Run()
+	defer client.Close()
+
 }
 
+func test(message, phone string) {
+	headers := map[string][]string{
+		"Content-Type": []string{"application/json"},
+		"Accept":       []string{"application/json"},
+		"APIKey":       []string{"56E4E650EF96812F367FB6096B806DE7"},
+	}
+	payload, _ := json.Marshal(map[string]string{
+		// Sender is optional
+		// "sender": "YOUR_SENDER",
+		"destination": phone,
+		"message":     message,
+	})
+	url := "https://api.nusasms.com/nusasms_api/1.0/whatsapp/message"
+	// For testing
+	// url := "https://dev.nusasms.com/nusasms_api/1.0/whatsapp/message"
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	req.Header = headers
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+	// ...
+}
